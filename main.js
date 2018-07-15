5b320596c0d21f22be101bfd1d4bc00a996cad1e
Richard Chen <richardchen910@gmail.com>
2018-07-15 10:52:26 -0700

    Added smooth scroll

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+/*
+var boxes = document.getElementsByClassName("box");
+
+for (i = 0; i < 5; ++i)
+{
+	boxes[i].addEventListener("mouseover", showMore);
+}
+
+function showMore(e) {
+	e.target.classList.add("hover");
+}
+*/
+
