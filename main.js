7fa98e46f7a32e381f55b4171be6a691211490cc
Richard Chen <richardchen910@gmail.com>
2018-07-30 21:23:29 -0700

    commit 7/30/18

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 
 
-/*
-var boxes = document.getElementsByClassName("box");
-
-for (i = 0; i < 5; ++i)
-{
-	boxes[i].addEventListener("mouseover", showMore);
+//smooth scroll
+sections = document.querySelectorAll(".smooth-scroll");
+function myFunction(i) {
+    sections[i].scrollIntoView({ 
+    	behavior: 'smooth'
+    });
 }
 
-function showMore(e) {
-	e.target.classList.add("hover");
-}
-*/
+
+//reveal content when user clicks on arrow
+$(document).ready(function() {
+	//reveal content when user clicks on arrow
+	$(".view").on("click", function() {
+		var box = $(this).closest(".box");
+		var reveal_content = box.find(".reveal-content");
+		var button = box.find(".view img");
+		if (reveal_content.css("height") == "0px") {
+			reveal_content.css("height", "100%");
+			button.attr("src", "images/arrow_close.jpg");
+		}
+		else {
+			reveal_content.css("height", "0");
+			button.attr("src", "images/arrow_open.jpg");
+			button.addClass("arrow");
+		}
+	});
+
+	//ensure image takes up entire box width
+	var box_width = $(".fit").closest(".box").css("width");
+		
+	
+});
+
+
+
+
 
